76bcce77583532f93b68ebcb033449058fa84431
Patrick Black <50429647+patrickmblack@users.noreply.github.com>
2023-11-10 15:36:46 -0800

    Fix trail starting at Balls position at Frame zero

--- main.gd
+++ main.gd
@@ -1,14 +1,11 @@
-extends Line2D
-
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var pos = get_parent().position
-	add_point(pos)
 	pass
