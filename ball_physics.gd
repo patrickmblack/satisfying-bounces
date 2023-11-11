f2cabacfbd69fa115507543d7ed314bbb5ef22a3
Patrick Black <50429647+patrickmblack@users.noreply.github.com>
2023-11-10 22:37:43 -0800

    Add ball that bounces off rigid bodies
    Signed-off-by: Patrick Black <50429647+patrickmblack@users.noreply.github.com>

--- ball_physics.gd
+++ ball_physics.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 class_name Ball
 
+@export var acceleration = 5
+@export var speed = 350
+@export var speed_inc = 10
+
+@export var vel = Vector2(3,2)
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var note = $note
+
+
+var start_pos: Vector2
+
+
 func _ready():
-	pass
-	
-func start(pos):
-	pass
+	velocity = vel
 
 func _physics_process(delta):
-	move_and_slide()
+	var collision = move_and_collide(velocity * speed * delta)
+	if (collision):
+		velocity = velocity.bounce(collision.get_normal())
+		speed += speed_inc
+		note.play()
+	
