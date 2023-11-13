1dff7590396e23f15821647669b10f22763f956a
Patrick Black <50429647+patrickmblack@users.noreply.github.com>
2023-11-12 17:41:10 -0800

    Signed-off-by: Patrick Black <50429647+patrickmblack@users.noreply.github.com>

--- spawn_points.gd
+++ spawn_points.gd
@@ -1,22 +1,45 @@
 @tool
-
 extends Node2D
 
+var marker_pos = []
 var markers = []
 @export var number_of_markers = 2
+
 var screen_size
-@export var pos: Vector2
+@export var start_pos: Vector2
+var pos: Vector2
+@export var pos_increment = 100
 var marker
+var num_markers
+var i
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pos = Vector2(69, 150)
+	start_pos = Vector2(75, 360)
+	update_markers()
+	i = 0
+	pass
+	
+func update_markers():
+	
+	marker_pos = []
+	markers = []
 	screen_size = get_viewport_rect().size
-	marker = Marker2D.new()
-	add_child(marker)
-
+	
+	
+	
+	for i in number_of_markers:
+		pos = Vector2(start_pos.x + (i * pos_increment), start_pos.y)
+		marker_pos.push_back(pos)
+		marker = Marker2D.new()
+		marker.position = pos
+		add_child(marker)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	marker.position = pos
+	if Engine.is_editor_hint():
+		if i == 0:
+			update_markers()
+			i += 1
