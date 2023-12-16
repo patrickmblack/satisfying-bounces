22d7e704fb4e83162de5caf728294b8bf4a09f77
Patrick Black <50429647+patrickmblack@users.noreply.github.com>
2023-12-15 19:10:11 -0800

    Change color of spawned ball, code gradient
    Ball spawns with eat hit at the position of the ball. Gradient is randomized.
    
    Signed-off-by: Patrick Black <50429647+patrickmblack@users.noreply.github.com>

--- ball_by_code.gd
+++ ball_by_code.gd
@@ -9,18 +9,33 @@ extends Node2D
 @export var trail_color: Color
 @onready var trail_fade = $trail_fade
 
+var random_color: Color
+var random_color_fade: Color
+
+signal wall_bounce
+
 
 var screen_size
 
 
 func _ready():
-	screen_size = get_viewport_rect().size
-	trail_fade.default_color = trail_color
+	var random_hue = randf()
+	random_color = Color.from_hsv(random_hue,1.0,1.0,1.0)
+	random_color_fade = Color.from_hsv(random_hue,1.0,1.0,0.0)
 	
+	screen_size = get_viewport_rect().size
+	#trail_fade.default_color = random_color
+	
+	var trail_gradient = Gradient.new()
+	
+	trail_gradient.add_point(0.0, random_color)
+	trail_gradient.add_point(0.999999, random_color_fade)
+	
+	trail_fade.gradient = trail_gradient
 
 func _draw():
 	draw_circle(Vector2.ZERO, radius, Color.WHITE)
-	draw_circle(Vector2.ZERO, radius-2, Color.BLACK)
+	draw_circle(Vector2.ZERO, radius-3, random_color)
 	
 
 func _process(delta):
@@ -31,13 +46,18 @@ func _process(delta):
 	if position.x < radius or position.x > get_viewport_rect().size.x - radius:
 		velocity.x *= -1  # Reverse the horizontal velocity
 		speed += speed_increment
+		emit_signal("wall_bounce")
+		print("bounce")
 		$note.play()
 
 	if position.y < radius or position.y > get_viewport_rect().size.y - radius:
 		velocity.y *= -1 # Reverse the vertical velocity
 		speed += speed_increment
+		emit_signal("wall_bounce")
+		print("bounce")
 		$note.play()
 
 	# Keep the circle inside the viewport
 	position.x = clamp(position.x, radius, get_viewport_rect().size.x - radius)
 	position.y = clamp(position.y, radius, get_viewport_rect().size.y - radius)
+
--- spawn_points.gd
+++ spawn_points.gd
@@ -1,45 +1,23 @@
-@tool
 extends Node2D
 
-var marker_pos = []
-var markers = []
-@export var number_of_markers = 2
-
-var screen_size
-@export var start_pos: Vector2
-var pos: Vector2
-@export var pos_increment = 100
-var marker
-var num_markers
-var i
-
+@export var ball_scene: PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_pos = Vector2(75, 360)
-	update_markers()
-	i = 0
 	pass
-	
-func update_markers():
-	
-	marker_pos = []
-	markers = []
-	screen_size = get_viewport_rect().size
-	
-	
-	
-	for i in number_of_markers:
-		pos = Vector2(start_pos.x + (i * pos_increment), start_pos.y)
-		marker_pos.push_back(pos)
-		marker = Marker2D.new()
-		marker.position = pos
-		add_child(marker)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Engine.is_editor_hint():
-		if i == 0:
-			update_markers()
-			i += 1
+	if Input.is_action_pressed("escape"):
+		get_tree().quit()
+
+
+func on_wall_bounce():
+	var ball = ball_scene.instantiate()
+	var spawn_location = get_node("ball_by_code")
+	
+	ball.position = spawn_location.position
+	ball.velocity = Vector2(randf_range(1, 3), randf_range(1, 3))
+	
+	add_child(ball)
